cb7af26bc2b16c2ba9506ecbbd7d01c59180a5d8
William Durand <will+git@drnd.me>
2017-09-03 21:52:12 +0200

    Configure the Redux devtools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,13 +2,27 @@ import { applyMiddleware, createStore } from 'redux';
 
 import rootReducer from 'reducers';
 
+const isNotProduction = process.env.NODE_ENV !== 'production';
+
 const middleware = [];
+
+if (isNotProduction) {
+  const { logger } = require('redux-logger');
+
+  middleware.push(logger);
+}
+
 const createStoreWithMiddleware = applyMiddleware(...middleware)(
   createStore
 );
 
 export default function configureStore(initialState) {
-  const store = createStoreWithMiddleware(rootReducer, initialState);
+  const store = createStoreWithMiddleware(
+    rootReducer,
+    initialState,
+    typeof window !== 'undefined' &&
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
   return store;
 }
