d8dfca9a116aa2a1287c93a8ed9891cb553e5c1c
William Durand <will+git@drnd.me>
2017-09-03 13:02:22 +0200

    Move sequences to the App state

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import List from 'List';
 import 'styles.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sequences: ['ATCG', 'ATCGATTT'],
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,7 +22,7 @@ class App extends Component {
           <div className="row">
             <div className="col-md-4">
               <h3>Sequences</h3>
-              <List sequences={['ATCG', 'ATCGATTT']} />
+              <List sequences={this.state.sequences} />
             </div>
           </div>
         </div>
