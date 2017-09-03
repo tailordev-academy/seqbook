4fdf4494f2343e33d34976979f1f0a0ed62c2086
William Durand <will+git@drnd.me>
2017-09-03 12:11:53 +0200

    Add Header component

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () =>
+  <nav className="navbar navbar-default navbar-fixed-top">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <a className="navbar-brand" href="/">
+          Seqbook
+        </a>
+      </div>
+    </div>
+  </nav>;
+
+export default Header;
