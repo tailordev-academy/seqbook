da264997838c9193b8440b59c7c124363699c449
William Durand <will+git@drnd.me>
2017-09-05 09:25:29 +0200

    Add Seqbook component

---
+++ src/Seqbook.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Home from 'Home';
+import About from 'About';
+import Header from 'Header';
+import NotFound from 'NotFound';
+
+export default () => (
+  <div>
+    <Header />
+
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route component={NotFound} />
+    </Switch>
+  </div>
+);
--- src/index.js
+++ src/index.js
@@ -2,19 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import 'isomorphic-fetch';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
-import Home from 'Home';
-import About from 'About';
-import Header from 'Header';
-import NotFound from 'NotFound';
+import Seqbook from 'Seqbook';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore from 'store/configureStore';
 
@@ -23,15 +16,7 @@ const store = configureStore();
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <div>
-        <Header />
-
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route component={NotFound} />
-        </Switch>
-      </div>
+      <Seqbook />
     </Router>
   </Provider>,
   document.getElementById('root')
