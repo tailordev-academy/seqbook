f5e23f6b5d1c1a994f9612f10e2aed6dd8081ef8
William Durand <will+git@drnd.me>
2017-09-07 22:27:37 +0200

    Add tests for Length component

---
+++ src/widgets/Length/index.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+
+import Card from 'ui/Card';
+
+import Length from './index';
+
+it('renders correctly', () => {
+  const sequence = 'ATCG';
+  const tree = renderer.create(
+    <Length sequence={sequence} />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('uses a Card', () => {
+  const wrapper = shallow(<Length sequence={''} />);
+  expect(wrapper.find(Card)).toHaveLength(1);
+});
