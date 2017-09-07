817864b66c4d72657a6007fca2a67fc4a5f5d1ff
William Durand <will+git@drnd.me>
2017-09-07 22:23:13 +0200

    Add snapshot tests for Card component

---
+++ src/ui/Card/index.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Card from './index';
+
+it('renders a card', () => {
+  const tree = renderer.create(
+    <Card title="card title" value="card value" />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('can display a unit', () => {
+  const tree = renderer.create(
+    <Card title="card title" value="10" unit="%" />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('can take numbers as value', () => {
+  const tree = renderer.create(
+    <Card title="card title" value={10} unit="%" />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
